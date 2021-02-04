218ace199822263fc2c1c1d7990d8eeb27f857cc
MrsG-ood <tamara.gontarek@gmail.com>
2021-02-04 12:49:24 -0800

    Final Functions added
    mistake- commented out script on html which led to hours spent debugging... code that was simply not linked. Lesson: double check the basics when JS suddenly doesnt work.

--- script.js
+++ script.js
@@ -1,44 +1,25 @@
-// when screen is =- than 992px
-// show burger icon
-// when burger icon is active - add .burger-active
-// classList to show navList & .close icon
-// when .closer icon is clicked remove active classList on navList
-// and .buger
+const burger = document.getElementById("burger");
+const mobileLinks = document.getElementById("mobileNavList");
+const close = document.getElementById("close");
+const logo = document.getElementById("logoText");
 
-// close buger menu 
-// const bugerOpenNav = document.querySelector('.burger-openNav');
-// show navList
-// 
-// const burger = document.getElementById('burger');
-// show close icon from font awesome
 
-// const closeIcon = document.querySelector('.far-active');
+function openNav() { 
+    mobileLinks.style.display = "block";
+    burger.style.display = "none";
+    logo.style.display = "none";
+};
 
-// burger.addEventListener("click", function () {
-//     // console.log( "This is a test");
-// //    mobileNav.classList.add("navList-active", "far-active","burger-openNav");
-//     mobileNav.style.display = "flex"; //shows navList
-//     burger.style.display = "none"; //removes burger icon
-//     close.style.display = "block";
-// });
-
-const burger = document.getElementById('burger');
-const mobileNav = document.getElementById('navList');
-const close = document.getElementById('close');
-const logo = document.getElementById('logoText');
-
-function closeNav(){
-    mobileNav.style.display = "none";
+function closeNav() { 
+    mobileLinks.style.display = "none";
     burger.style.display = "block";
     logo.style.display = "flex";
-}
-function openNav() { 
-            mobileNav.style.display = "flex"; //shows navList and close icon
-            burger.style.display = "none"; //removes burger icon
-            logo.style.display = "none"; //removes logo
-}
-close.addEventListener("click", closeNav, false);
-burger.addEventListener("click", openNav, false);
+};
+close.addEventListener("click", closeNav);
+
+burger.addEventListener("click", openNav);
+
+
 
 
 
